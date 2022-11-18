c52cbb324075c0286e89d2c7a57fa780676e4f06
sk230144 <95127073+sk230144@users.noreply.github.com>
2022-11-18 05:27:16 -0800

    Add files via upload

---
+++ thapa website/index.js
@@ -0,0 +1,10 @@
+const mobile_nav = document.querySelector(".mobile-navbar-btn");
+const nav_header = document.querySelector(".header");
+const toggleNavbar = () => {
+
+ // alert("hi");
+
+nav_header.classList.toggle("active"); 7 };
+mobile_nav.addEventListener("click", () toggleNavbar());
+
+10
